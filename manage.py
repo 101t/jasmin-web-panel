c0edb550868de508e3bd5c9945ef7d9c4ab3df5e
101t <tarek.it.eng@gmail.com>
2022-06-04 20:07:40 +0300

    SUBMIT_LOG config added

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv("DJANGO_SETTINGS_MODULE", "'config.settings.dev'"))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv("DJANGO_SETTINGS_MODULE", "config.settings.dev"))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
