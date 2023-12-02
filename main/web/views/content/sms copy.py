02f3fdaf5f1a068038a9842c88933248e01d83c0
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2023-12-02 21:43:49 +0200

    update

--- main/web/views/content/sms copy.py
+++ main/web/views/content/sms copy.py
@@ -5,9 +5,9 @@ from django.contrib.auth.decorators import login_required
 
 import sys
 
-import urllib.request, urllib.error, urllib.parse
-import urllib.request, urllib.parse, urllib.error
-
+import smpplib.gsm
+import smpplib.client
+import smpplib.consts
 from time import sleep
 import json
 
@@ -52,8 +52,35 @@ def send_sms_view_manage(request):
     args['sms'] = request.POST.get("sms")
     callee = request.POST.get("msisdn")
     sms = request.POST.get("sms")
-    baseParams = {'username':'foo', 'password':'bar', 'from': 'jasmin', 'to': callee, 'content': sms}
-    res = urllib.request.urlopen("http://172.22.0.4:1401/send?%s" % urllib.parse.urlencode(baseParams)).read().decode('utf-8')
-    res_status = 200
-    args['result'] = res
+    client = smpplib.client.Client('172.22.0.31', 2775)
+    client.set_message_sent_handler(sent_handler)
+    client.set_message_received_handler(received_handler)
+    client.connect()
+    client.bind_transceiver(system_id='test', password='test')
+    parts, encoding_flag, msg_type_flag = smpplib.gsm.make_parts(sms)
+    #logging.info('Sending SMS "%s" to %s' % (string, dest))
+    for part in parts:
+        pdu = client.send_message(
+            source_addr_ton=smpplib.consts.SMPP_TON_SBSCR,
+            source_addr_npi=smpplib.consts.SMPP_NPI_ISDN,
+            source_addr="jasmin",
+            dest_addr_ton=smpplib.consts.SMPP_TON_SBSCR,
+            dest_addr_npi=smpplib.consts.SMPP_NPI_ISDN,
+            destination_addr=callee,
+            short_message=part,
+            data_coding=encoding_flag,
+            #esm_class=msg_type_flag,
+            esm_class=smpplib.consts.SMPP_MSGMODE_FORWARD,
+            registered_delivery=False,
+    )
+    sleep(.2)
+    try:
+        client.read_once()
+        res_stats = 200
+    except Exception as e:
+        res_message = str(e)
+        rest_status = 500
+        args['error'] = res_message
+    sleep(.8)
+    client.unbind()
     return HttpResponse(json.dumps(args), status=res_status, content_type="application/json")
