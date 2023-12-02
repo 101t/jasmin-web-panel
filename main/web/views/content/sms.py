cef6de1d4e8ce8720d9c85dc9a3d416b61febfd2
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2023-12-02 09:16:09 +0100

    udpate

--- main/web/views/content/sms.py
+++ main/web/views/content/sms.py
@@ -16,28 +16,5 @@ def send_sms_view(request):
 @login_required
 def send_sms_view_manage(request):
     args, res_status, res_message = {}, 400, _("Sorry, Command does not matched.")
-    httpccm = None
-    if request.POST and request.is_ajax():
-        s = request.POST.get("s")
-        if s in ['list', 'add', 'delete']:
-            httpccm = HTTPCCM(telnet=request.telnet)
-        if httpccm:
-            if s == "list":
-                args = httpccm.list()
-                res_status, res_message = 200, _("OK")
-            elif s == "add":
-                httpccm.create(data=dict(
-                    cid=request.POST.get("cid"),
-                    url=request.POST.get("url"),
-                    method=request.POST.get("method"),
-                ))
-                res_status, res_message = 200, _("HTTPCCM added successfully!")
-            elif s == "delete":
-                args = httpccm.destroy(cid=request.POST.get("cid"))
-                res_status, res_message = 200, _("HTTPCCM deleted successfully!")
-    if isinstance(args, dict):
-        args["status"] = res_status
-        args["message"] = str(res_message)
-    else:
-        res_status = 200
+    res_status = 200
     return HttpResponse(json.dumps(args), status=res_status, content_type="application/json")
