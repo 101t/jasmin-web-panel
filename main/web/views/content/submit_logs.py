9e5628090bd2b735b320b41f2bd007f2ecd9dfcf
101t <tarek.it.eng@gmail.com>
2025-04-17 11:29:13 +0300

    fixing states on submit_log

--- main/web/views/content/submit_logs.py
+++ main/web/views/content/submit_logs.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.db.models import Count, Q
+from django.db.models import Count, Case, When, IntegerField
 
 from main.core.models import SubmitLog
 from main.core.utils import paginate
@@ -10,14 +10,19 @@ from main.core.tools import require_ajax
 
 @login_required
 def submit_logs_view(request):
-    collection_list = SubmitLog.objects.annotate(
-        success_count=Count("status", filter=Q(status= "success")),
-        fail_count=Count("status", filter=Q(status= "fail")),
-        unknown_count=Count("status", filter=Q(status= "unknown")),
-        total_count=Count("status"),
-    ).order_by("-created_at")
-    collection_list = paginate(collection_list, per_page=25, page=request.GET.get("page"))
-    return render(request, "web/content/submit_logs.html", dict(collection_list=collection_list))
+    stats = SubmitLog.objects.aggregate(
+        total_count=Count('id'),
+        success_count=Count(Case(When(status="success", then=1), output_field=IntegerField())),
+        fail_count=Count(Case(When(status="fail", then=1), output_field=IntegerField())),
+        unknown_count=Count(Case(When(status="unknown", then=1), output_field=IntegerField())),
+    )
+    submit_logs = SubmitLog.objects.order_by("-created_at")
+
+    submit_logs = paginate(submit_logs, per_page=25, page=request.GET.get("page"))
+    return render(request, "web/content/submit_logs.html", context={
+        "submit_logs": submit_logs,
+        "stats": stats,
+    })
 
 
 @require_ajax
