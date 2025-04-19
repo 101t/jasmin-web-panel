ed7e7f2d10318d01fada19d02dd387839d9917b1
101t <tarek.it.eng@gmail.com>
2025-04-19 21:27:05 +0300

    update deployment

--- config/docker/slim/docker-entrypoint.sh
+++ config/docker/slim/docker-entrypoint.sh
@@ -5,21 +5,22 @@ set -e
 APP_PORT=${APP_PORT:-8000}
 APP_DIR=${APP_DIR:-'/app'}
 APP_LOG_LEVEL=${APP_LOG_LEVEL:-'warn'}
-# APP_WSGI: 'wsgi' or 'asgi'
+
 APP_WSGI=${APP_WSGI:-'wsgi'}
-# APP_WORKER_CLASS: 'gevent' or 'uvicorn.workers.UvicornWorker'
+
 APP_WORKER_CLASS=${APP_WORKER_CLASS:-'sync'}
 APP_WORKERS=${APP_WORKERS:-4}
 
 cd "$APP_DIR"
 
+# Optional (not strictly needed if PATH is set in Dockerfile)
 source "$APP_DIR"/env/bin/activate
 
 python manage.py migrate
 python manage.py samples
-python manage.py collectstatic --noinput --clear --no-post-process
+python manage.py collectstatic --noinput --clear
 
-"$APP_DIR"/env/bin/gunicorn config."$APP_WSGI":application \
+exec "$APP_DIR"/env/bin/gunicorn config."$APP_WSGI":application \
   --workers "$APP_WORKERS" \
   --bind :"$APP_PORT" \
   --log-level "$APP_LOG_LEVEL" \
